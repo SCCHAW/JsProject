ab9c0d0492ff9773cfd499af70db677152a712cb
sweeney <sweeneychaw@email.com>
2024-07-26 15:08:02 +0800

    add new example for array.map()

--- BasicJS/basic3.js
+++ BasicJS/basic3.js
@@ -287,3 +287,44 @@ function randomwholeNum (){
     return Math.floor(Math.random() * 10)
 }
 console.log(randomwholeNum())
+
+//array - .map() = accepts a call back and applies that function to each element of an array, then return a new array.
+const num = [1, 2, 3, 4, 5]
+const squares = num.map(square)
+const cubes = num.map(cube)
+console.log(cubes)
+console.log(squares)
+
+function square(element){
+    return Math.pow(element, 2)
+}
+
+function cube(element){
+    return Math.pow(element, 3)
+}
+
+// ex 2
+const students = ["Spongebob", "Patrick", "Squidward", "Sandy"];
+const studentsUpper = students.map(upperCase);
+const studentsLower = students.map(lowerCase);
+
+console.log(studentsLower);
+
+function upperCase(element){
+    return element.toUpperCase();
+}
+
+function lowerCase(element){
+    return element.toLowerCase();
+}
+
+//ex 3
+const dates = ["2024-1-10", "2025-2-20", "2026-3-30"];
+const formattedDates = dates.map(formatDates);
+
+console.log(formattedDates);
+
+function formatDates(element){
+    const parts = element.split("-");
+    return `${parts[1]}/${parts[2]}/${parts[0]}`;
+}
